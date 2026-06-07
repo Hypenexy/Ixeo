2a12ba4032cec1afd88275266ab1b80405ddd596
Hypenexy <61563710+Hypenexy@users.noreply.github.com>
2026-06-07 16:21:38 +0300

    Removed legacy code from main branch

--- dark.js
+++
@@ -1,4 +0,0 @@
-
-var style = document.createElement('style');
-style.innerHTML = 'body{background-color: #000}input,button{background:#000;color:#fff}#lo{background:#090909!important;color:#fff;border-top: 1px solid #333!important;}#secondrow textarea{color:#fff}';
-document.head.appendChild(style);
--- experiments/older experiments/idk/request.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "<div class='site'>putko</div>";
